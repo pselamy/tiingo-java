1f06fc9ebf98e9b3a05ec9c5e971a9f0f59c22cf
Patrick Selamy <pselamy@gmail.com>
2022-10-13 00:02:50 -0400

    Remove examples.

--- src/main/java/example/HelloWorldLogger.java
+++
@@ -1,9 +0,0 @@
-package example;
-
-import com.google.common.collect.ImmutableList;
-
-interface HelloWorldLogger {
-    void logMessage(String message);
-
-    ImmutableList<String> loggedMessages();
-}
