e9924b1b5ae5d60d44688868e2ea36e9370cb377
Patrick Selamy <pselamy@gmail.com>
2022-10-16 19:15:40 -0400

    Update `Candle` spec.

--- src/main/java/com/github/pselamy/tiingo/api/models/Candle.java
+++ src/main/java/com/github/pselamy/tiingo/api/models/Candle.java
@@ -1,9 +1,15 @@
 package com.github.pselamy.tiingo.api.models;
 
-import java.math.BigDecimal;
-import java.time.Instant;
+import com.google.auto.value.AutoValue;
 
+import java.math.BigDecimal;
+
+@AutoValue
 public abstract class Candle {
+  public static Builder builder() {
+    return new AutoValue_Candle.Builder();
+  }
+
   public abstract BigDecimal open();
 
   public abstract BigDecimal high();
@@ -14,5 +20,22 @@ public abstract class Candle {
 
   public abstract BigDecimal volume();
 
-  public abstract Instant start();
+  public abstract String date();
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder open(BigDecimal open);
+
+    public abstract Builder high(BigDecimal high);
+
+    public abstract Builder low(BigDecimal low);
+
+    public abstract Builder close(BigDecimal close);
+
+    public abstract Builder volume(BigDecimal volume);
+
+    public abstract Builder date(String date);
+
+    public abstract Candle build();
+  }
 }
