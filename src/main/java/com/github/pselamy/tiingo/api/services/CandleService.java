9dca26fb75f3ff5fae2dd7fea6014e124de96f99
Patrick Selamy <pselamy@gmail.com>
2022-10-16 23:55:24 -0400

    Add `CandleService`

---
+++ src/main/java/com/github/pselamy/tiingo/api/services/CandleService.java
@@ -0,0 +1,50 @@
+package com.github.pselamy.tiingo.api.services;
+
+import com.github.pselamy.tiingo.api.models.AssetType;
+import com.github.pselamy.tiingo.api.models.CandleResponse;
+import com.github.pselamy.tiingo.api.models.Granularity;
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSortedSet;
+
+import java.time.Instant;
+import java.util.Optional;
+
+public interface CandleService {
+  ImmutableList<CandleResponse> getCandles(GetCandlesRequest getCandlesRequest);
+
+  AssetType assetType();
+
+  @AutoValue
+  abstract class GetCandlesRequest {
+    public static Builder builder() {
+      return new AutoValue_CandleService_GetCandlesRequest.Builder();
+    }
+
+    abstract ImmutableSortedSet<String> tickers();
+
+    abstract Optional<Instant> startDate();
+
+    abstract Optional<Instant> endDate();
+
+    abstract Granularity granularity();
+
+    @AutoValue.Builder
+    public abstract static class Builder {
+      abstract ImmutableSortedSet.Builder<String> tickersBuilder();
+
+      public Builder addTicker(String ticker) {
+        tickersBuilder().add(ticker);
+        return this;
+      }
+
+      public abstract Builder startDate(Instant startDate);
+
+      public abstract Builder endDate(Instant endDate);
+
+      public abstract Builder granularity(Granularity granularity);
+
+      public abstract GetCandlesRequest build();
+    }
+  }
+}
---
+++ src/test/java/com/github/pselamy/tiingo/api/services/CryptoCandleServiceTest.java
@@ -0,0 +1,50 @@
+package com.github.pselamy.tiingo.api.services;
+
+import com.github.pselamy.tiingo.api.models.ApiKey;
+import com.github.pselamy.tiingo.api.models.CandleResponse;
+import com.github.pselamy.tiingo.api.models.Granularity;
+import com.github.pselamy.tiingo.api.rest.RestClientModule;
+import com.github.pselamy.tiingo.api.services.CandleService.GetCandlesRequest;
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.testing.fieldbinder.Bind;
+import com.google.inject.testing.fieldbinder.BoundFieldModule;
+import com.google.testing.junit.testparameterinjector.TestParameterInjector;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.time.Instant;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@RunWith(TestParameterInjector.class)
+public class CryptoCandleServiceTest {
+  @Bind
+  private static final ApiKey API_KEY = ApiKey.create("98908a4d329d2781e2c1cdcd51cb9dbb4c090705");
+
+  @Inject private CryptoCandleService service;
+
+  @Before
+  public void setUp() {
+    Guice.createInjector(BoundFieldModule.of(this), RestClientModule.create()).injectMembers(this);
+  }
+
+  @Test
+  public void getCandles_returnsCandles() {
+    // Arrange
+    GetCandlesRequest request =
+        GetCandlesRequest.builder()
+            .addTicker("BTCUSD")
+            .endDate(Instant.now())
+            .granularity(Granularity.FIVE_MINUTES)
+            .build();
+
+    // Act
+    ImmutableList<CandleResponse> actual = service.getCandles(request);
+
+    // Assert
+    assertThat(actual).isNotEmpty();
+  }
+}
