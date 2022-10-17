fb18c671a4ea3646948a3e84c373def0899a2a74
Patrick Selamy <pselamy@gmail.com>
2022-10-16 23:53:16 -0400

    Implement `TiingoClient`.

--- src/main/java/com/github/pselamy/tiingo/api/client/TiingoClient.java
+++ src/main/java/com/github/pselamy/tiingo/api/client/TiingoClient.java
@@ -1,5 +1,20 @@
 package com.github.pselamy.tiingo.api.client;
 
-abstract class TiingoClient {
-  abstract String apiKey();
+import com.github.pselamy.tiingo.api.services.CandleService;
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.Inject;
+
+import com.github.pselamy.tiingo.api.models.AssetType;
+
+public class TiingoClient {
+  private final ImmutableMap<AssetType, CandleService> candleServices;
+
+  @Inject
+  TiingoClient(ImmutableMap<AssetType, CandleService> candleServices) {
+    this.candleServices = candleServices;
+  }
+
+  public CandleService getCandleService(AssetType assetType) {
+    return candleServices.get(assetType);
+  }
 }
